32f9fe6564c1f4c70e14ded081372378c1d53120
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-08-28 15:20:22 +0200

    chessApp 1.0

---
+++ Chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// ░ och ▓
+// 8x8 for loop 
+// börjar på vitt 
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int column = 0; column < 8; column++)  
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    if ((row + column) % 2 == 0)
+                    {
+                        Console.Write("▓▓");
+                    }
+                    else if ((row + column ) % 2 == 1)
+                    {
+                        Console.Write("░░");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
