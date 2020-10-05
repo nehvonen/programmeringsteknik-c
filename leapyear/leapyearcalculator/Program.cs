9cfa7b65926c729024fb566633e46b5e8d7fb4b4
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-10-05 09:35:45 +0200

    Cleaned code

--- leapyear/leapyearcalculator/Program.cs
+++ leapyear/leapyearcalculator/Program.cs
@@ -21,9 +21,6 @@ namespace leapyearcalculator
                     Console.WriteLine("{0} is a leap year.", year);
                     DateTime leapDay = new DateTime(year, 2, 29);
                     DateTime nextYear = leapDay.AddYears(1);
-                    Console.WriteLine("  Leap year from {0} is {1}.",
-                                      leapDay.ToString("d"),
-                                      nextYear.ToString("d"));
                 }
             }
         }
