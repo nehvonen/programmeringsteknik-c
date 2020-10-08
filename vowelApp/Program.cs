d4052d405ccdfde0dcd4da209e1e3153c1706254
Nehvonen <https://github.com/nehvonen>
2020-10-08 09:53:41 +0200

    vowelApp finished

---
+++ vowelApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vowelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write string of text");
+            var input = Console.ReadLine();
+            var letters = new HashSet<char>(input);
+            letters.ExceptWith("aeiou");
+            Console.WriteLine("Here is your string of text without vowels.");
+            foreach (char c in letters) Console.Write(c);
+            
+
+
+        }
+    }
+}
