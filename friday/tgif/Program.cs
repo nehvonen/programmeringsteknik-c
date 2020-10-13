cb1c25dc9775f20b63e1efd947f76c870be8c7b8
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-10-13 10:11:04 +0200

    Date calc

--- friday/tgif/Program.cs
+++ friday/tgif/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Globalization;
 
 namespace tgif
 {
@@ -8,6 +10,28 @@ namespace tgif
         {
             // Skriv en applikation som läser in ett datum via användarinmatning,
             // som sedan räknar ut hur många dagar det är till nästa fredag.
+            
+            
+            DateTime userDateTime;
+            while (!GetDateFromUser(out userDateTime));
+
+            DayOfWeek givenDay = userDateTime.DayOfWeek;
+            DayOfWeek friday = DayOfWeek.Friday;
+
+            int difference = friday - givenDay;
+            if (difference < 0)
+                difference += 7;
+            
+            Console.WriteLine($"It's friday in {difference} days");
+        }
+        static bool GetDateFromUser(out DateTime userDateTime)
+        {
+            Console.WriteLine("Skriv ett datum.");
+            string userInput = Console.ReadLine();
+
+            CultureInfo culture = new CultureInfo("sv-SE");
+
+            return DateTime.TryParse(userInput, culture, DateTimeStyles.AssumeLocal, out userDateTime);
         }
     }
 }
