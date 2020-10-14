6eb7ee317ed2947ab8668bf3352b51c638b4068a
Nehvonen <nehvonen@protonmail.com>
2020-10-14 09:36:16 +0200

    Number to month 1.0

--- months/months/Program.cs
+++ months/months/Program.cs
@@ -10,6 +10,24 @@ namespace months
             // Skriv ett program som tar en emot inmatad siffra (1-12)
             // och konverterar siffran till ett månadsnamn på svenska
             // programmet skall kasta ett fel om den inmatade siffran är något annat än 1-12.
+            CultureInfo cultureInfo = new CultureInfo("sv-SE");
+
+            Console.WriteLine("Skriv en siffra 1-12");
+            var userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+           
+            
+            if (number < 1 || number > 12)
+            {
+                string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(number);
+                Console.WriteLine(monthName);
+                Console.WriteLine("Your input was incorrect.");
+            }
+            else 
+            {
+                string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(number);
+                Console.WriteLine(monthName);
+            }
         }
     }
 }
